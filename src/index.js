079572128aca0c86173d1126d641c4a7e18bb81a
Arnav Savant <savantishu02@gmail.com>
2023-07-05 22:34:26 +0530

    Implemented transport object for nodemailer and tested it

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const {serverConfig, Logger} = require('./config');
+const {serverConfig, Logger, mailSender} = require('./config');
 const routes = require('./routes');
 
 const app = express();
@@ -10,6 +10,17 @@ app.use(express.urlencoded());
 
 app.use('/api',routes);
 
-app.listen(serverConfig.PORT, () => {
+app.listen(serverConfig.PORT, async() => {
 	console.log(`Succesfully listening on PORT: ${serverConfig.PORT}`);
+	try {
+		const response = await mailSender.sendMail({
+			from: serverConfig.GMAIL_EMAIL,
+			to: "savantishu02@gmail.com",
+			subject: "Is the service working ?",
+			text: "Yes, it is working",
+		});
+		console.log(response);
+	} catch (error) {
+		console.log(error);
+	}
 });
